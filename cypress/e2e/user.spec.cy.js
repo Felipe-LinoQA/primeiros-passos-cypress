be0ac2f3b77cdb7673a9a226c230b288a3457489
Felipe Lino <felipelinosilva@outlook.com>
2026-05-19 14:40:10 -0300

    add generic fild

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,16 +7,31 @@ describe('Orange HRM Tests', () => {
     passwordField: "[name='password']",
     loginButton: 'button',
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert"
+    wrongCredentialAlert: ".oxd-alert",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    clickOutField: ".oxd-layout-context",
+    myNationality: "[clear='false']"
   }
 
- it('Login - Sucess', () => {
+ it.only('User Info Updade - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('testName') 
+    cy.get(selectorsList.lastNameField).clear().type('testLastName')
+    cy.get(selectorsList.genericField).eq(3).clear().type('LdTest')
+    cy.get(selectorsList.genericField).eq(4).clear().type('otherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('driversTeste')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-15-03')
+    cy.get(selectorsList.clickOutField).click()
+    cy.get(selectorsList.myNationality).eq(0).click()
   })
 
   it('login - fail', () => {
